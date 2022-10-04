15ca2e1c06f73cd5a768592eee78b11c50f852e6
Raghad Alameel <114874809+RaghadAlameel@users.noreply.github.com>
2022-10-04 15:17:07 +0300

    Add files via upload

---
+++ Assignment2a.kt
@@ -0,0 +1,14 @@
+package com.example.firstproject
+
+fun main() {
+
+    for(counter in 1..100)
+    {
+        if(counter ==71)
+        {
+            println("IT'S OVER 70!!!")
+        }
+        println(counter)
+
+    }
+}
